14c2fdf20972d76ec0f7e051ffd825031592643d
Pedro <pedrohk@gmail.com>
2025-08-06 00:48:44 -0300

    ajusts

--- sample-app/main.go
+++ sample-app/main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, DevOps Kata!")
+	w.Write([]byte("Hello, DevOps Kata!"))
 }
 
-func main() {
-  http.HandleFunc("/", handler)
-  http.Handle("/metrics", promhttp.Handler())
-  http.ListenAndServe(":8080", nil)
+func TestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "Hello, DevOps Kata!"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
